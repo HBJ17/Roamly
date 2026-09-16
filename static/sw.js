511e2a8493e5a8f1ef547be9465202d8e07a97fa
HBJ17 <hbj17672007@gmail.com>
2026-09-16 15:45:02 +0530

    feat: scannable QR boarding passes, PWA offline manifest and service worker

---
+++ static/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'roamly-v2-cache';
+const STATIC_ASSETS = [
+  '/',
+  '/static/css/style.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS).catch(err => console.log('Static asset caching notice:', err));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
